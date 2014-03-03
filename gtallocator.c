f8b8005b1b0f80771019cc87bfebdbab818d6a79
Alex Saltiel <alex.saltiel@gmail.com>
2014-03-02 21:40:51 -0500

    Created initial struct and constants. Tree mgmt functions still needs to be outlined. Consider finding a library for tree structure and mgmt

--- gtallocator.c
+++ gtallocator.c
@@ -1,13 +1,36 @@
-#include <stdlib.h>
-#include <unistd.h> //for brk & sbrk
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h> //for brk & sbrk
 
+/*
+	TODO: 
+		- DO EVERYTHING IMPORTANT ATOMICALLY
+		- Initialize prg_mem to mmapped block for tree structure 
+		- Initialize user_mem to mmapped block of INITIAL_BLOCK
+		- Create functions to resize program and user space
+*/
 
 void * gtalloc(size_t bytes){
+	/*
+		TODO:
+			- Implement best-fit algorithm to find and assign memory
+			- Update memory tree
+			- Log user allocation
+			- Return usable pointer to user
+	*/
     if (bytes == 0) {
 	return NULL;
     }
 }
 
 void gtfree(void * addr){
-
+	/*
+		TODO:
+			- Mark unallocated block as free
+			(recursively)
+			- Check buddy partitions
+			- Merge if buddy is currently free
+	*/
 }
--- gtallocator.h
+++ gtallocator.h
@@ -2,6 +2,30 @@
 #define _GT_ALLOC
 #endif
 
+// Size (in bytes) of initial or new mmapped memory chucks
+// Example on wikipedia is 1024 KB
+#define INITIAL_BLOCK 1048576
+
+// Defined stats for a tree node
+#define EMPTY 0
+#define PARTIALLY_OCCUPIED 1
+#define OCCUPIED 2
+
+/*
+	TODO: Outline what thread locks are needed
+		- I think we should lock almost everything
+*/
+
+struct node {
+	uint32_t state;
+	uint32_t size;
+	struct node *left_child;
+	struct node *right_child;
+};
+
+// global variable to store allocated memory
+extern void *user_mem;
+extern void *prg_mem;
 
 void * gtalloc(size_t bytes);
 
